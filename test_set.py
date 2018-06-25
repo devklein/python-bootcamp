5f02e24190e846b7fc3107510eca8a9c0307bd30
Stefan Klein <stefan@devklein.com>
2018-06-25 22:12:17 +0200

    update solution

--- test_set.py
+++ test_set.py
@@ -1,20 +1,19 @@
 # check how many different firstnames are in names.csv
 # Format of csv file: Id, Name, Year, Gender, State, Count
 
-import csv
-
 with open("./data/names.csv", "r") as file:
 
     names = set()
+    line_counter = 0
 
     for line in file:
         # putting the csv elements in a list, remove linebreaks
         splitted_line = line.strip().split(",")      
-        if "Id" not in splitted_line:
+        # skip header line
+        if line_counter != 0:
             names.add(splitted_line[1])
-        else:
-            # jumping over line 1 (header)
-            continue
+        line_counter += 1
+        
 
 # print result        
 print(len(names))
